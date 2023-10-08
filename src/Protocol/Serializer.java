ffbf4276690ef1a73b1aa51cccc9ab24483547b8
WangYiiiiF <wangyiiiif@163.com>
2023-10-08 19:20:21 +0800

    Initial commit

--- src/Protocol/Serializer.java
+++ src/Protocol/Serializer.java
@@ -46,35 +46,16 @@ public interface Serializer {
         Json {
             @Override
             public <T> T deserialize(Class<T> clazz, byte[] bytes) {
-                Gson gson = new GsonBuilder().registerTypeAdapter(Class.class, new ClassCodec()).create();
                 String json = new String(bytes, StandardCharsets.UTF_8);
-                return gson.fromJson(json, clazz);
+                return new Gson().fromJson(json, clazz);
             }
 
             @Override
             public <T> byte[] serialize(T object) {
-                Gson gson = new GsonBuilder().registerTypeAdapter(Class.class, new ClassCodec()).create();
-                String json = gson.toJson(object);
+                String json = new Gson().toJson(object);
                 return json.getBytes(StandardCharsets.UTF_8);
             }
         }
     }
-    class ClassCodec implements JsonSerializer<Class<?>>, JsonDeserializer<Class<?>> {
 
-        @Override
-        public Class<?> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-            try {
-                String str = json.getAsString();
-                return Class.forName(str);
-            } catch (ClassNotFoundException e) {
-                throw new JsonParseException(e);
-            }
-        }
-
-        @Override             //   String.class
-        public JsonElement serialize(Class<?> src, Type typeOfSrc, JsonSerializationContext context) {
-            // class -> json
-            return new JsonPrimitive(src.getName());
-        }
-    }
 }
